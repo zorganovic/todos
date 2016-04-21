2d11f16d0f74b9cc7d0ec7111cffbb36a3a20f0a
zorganovic <lazreg@gmail.com>
2016-04-21 11:44:39 +0100

    finished

---
+++ routes/todos/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+ var Todo = require('../../models/todos');
+ router.get('/', function(req, res, next) {
+ 	Todo.findAsync()
+    .then(function(todos) {
+      res.render('todos', {title: 'Todos', todos: todos});
+    })
+    .catch(next)
+    .error(console.error);
+ });
+
+module.exports = router;
